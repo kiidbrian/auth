0067f799e43a1bfd6529d3c4b61450b41201456f
kiidbrian <brnpaintsil@gmail.com>
2017-06-26 16:06:06 -0700

    adds firebase for auth

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBSWxiKda8vMKDHJtmO3YGySMam-2gec4o',
+      authDomain: 'auth-83b55.firebaseapp.com',
+      databaseURL: 'https://auth-83b55.firebaseio.com',
+      projectId: 'auth-83b55',
+      storageBucket: 'auth-83b55.appspot.com',
+      messagingSenderId: '336129973935'
+    });
+  }
+
   render() {
     return (
       <View>
